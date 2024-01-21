36faa99ac5441f43af890ea748d73680517cad83
Eduardo Lopes <505902+duca@users.noreply.github.com>
2024-01-21 03:24:38 -0300

    first few solutions

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (quantity: i32) -> i32{
+    let price: i32 = if quantity <= 40 {2} else {1};
+
+    return quantity * price;
+}
 
 // Don't modify this function!
 #[test]
